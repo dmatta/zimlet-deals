ffab7dc35c061bd6224ad3e83a388e21221139a8
Jon Mead <jmead@XXXX-XXmbpr.local>
2018-03-29 17:09:15 -0400

    Added basic styles and coupon entry

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,16 +4,16 @@ import { withIntl } from '../../enhancers';
 import wire from 'wiretie';
 import style from './style';
 
-import Search from '../search';
+// import Search from '../search';
 
 export default function createApp(context) {
 
 	@withIntl
 	@provide({ zimbraComponents: context.components })
-	@wire('zimbraComponents', null, ({ Sidebar }) => ({ Sidebar }))
+	@wire('zimbraComponents', null, ( { Sidebar, Button, Icon }) => ({ Sidebar, Button, Icon }))
 	class App extends Component {
 
-		render({ Sidebar }) {
+		render({ Sidebar, Button, Icon }) {
 			return (
 				<div class={style.wrapper}>
 					{/*Example of using component from ZimbraX client, in this case, Sidebar*/}
@@ -29,7 +29,14 @@ export default function createApp(context) {
 						</ol>
 					</Sidebar>
 					<div class={style.main}>
-						<Search />
+						<ul class={style.deals}>
+							<li class={style.deal}>
+								<span class={style.dealLeft}><Icon name="archive" /></span>
+								<span class={style.dealMiddle}><a href="#">Bluefly:Save $10 when you spend $40 on household essentials</a><br/>Expires: 2018-Apr-15 
+								</span>
+								<span class={style.dealRight}><Button styleType="primary" brand="primary>">Save</Button> <Button styleType="secondary">Unsubscribe</Button></span>
+							</li>
+						</ul>
 					</div>
 				</div>
 			);
