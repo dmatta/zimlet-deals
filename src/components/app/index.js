03cd68b39f5aca7c3c57da0b6a8874bf246d36b8
Jon Mead <jmead@XXXX-XXmbpr.local>
2018-03-29 17:15:13 -0400

    basic coupon item

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,16 +4,16 @@ import { withIntl } from '../../enhancers';
 import wire from 'wiretie';
 import style from './style';
 
-// import Search from '../search';
+import Search from '../search';
 
 export default function createApp(context) {
 
 	@withIntl
 	@provide({ zimbraComponents: context.components })
-	@wire('zimbraComponents', null, ( { Sidebar, Button, Icon }) => ({ Sidebar, Button, Icon }))
+	@wire('zimbraComponents', null, ({ Sidebar }) => ({ Sidebar }))
 	class App extends Component {
 
-		render({ Sidebar, Button, Icon }) {
+		render({ Sidebar }) {
 			return (
 				<div class={style.wrapper}>
 					{/*Example of using component from ZimbraX client, in this case, Sidebar*/}
@@ -29,13 +29,8 @@ export default function createApp(context) {
 						</ol>
 					</Sidebar>
 					<div class={style.main}>
-						<ul class={style.deals}>
-							<li class={style.deal}>
-								<span class={style.dealLeft}><Icon name="archive" /></span>
-								<span class={style.dealMiddle}><a href="#">Bluefly:Save $10 when you spend $40 on household essentials</a><br/>Expires: 2018-Apr-15 
-								</span>
-								<span class={style.dealRight}><Button styleType="primary" brand="primary>">Save</Button> <Button styleType="secondary">Unsubscribe</Button></span>
-							</li>
+						<ul id="coupon-list">
+							<li><a id="hi" href="#">Bluefly:Save $10 when you spend $40 on household essentials</a></li>
 						</ul>
 					</div>
 				</div>
